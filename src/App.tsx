4417eec116d04c6351f99d5e001e81c445f46fc0
omlnaut <oneironaut.oml@gmail.com>
2026-05-05 19:25:55 +0000

    Counter example

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,16 @@
+import { useState } from "react";
 import { RecipeRow } from "./components/RecipeRow";
+
+function Counter() {
+  const [count, setCount] = useState(0);
+  return <button onClick={() => setCount(count + 1)}>{count}</button>;
+}
 
 function App() {
   return (
     <div>
       <h1>Recipe Migrator</h1>
+      <Counter></Counter>
       <RecipeRow
         recipe={{ id: "1", name: "first" }}
         selected={true}
