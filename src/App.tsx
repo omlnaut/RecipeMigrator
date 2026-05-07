4ba1218de783bee30bf2658db1289d09cff59dc0
omlnaut <oneironaut.oml@gmail.com>
2026-05-07 18:54:37 +0000

    button disable exercise

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,29 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { RecipeRow } from "./components/RecipeRow";
 import type { RecipeSummary } from "./types/recipe";
 
 function App() {
   const [selectedIds, setSelectedIds] = useState<string[]>([]);
+  const [text, setText] = useState<string>("not loaded");
+  const [shouldLoad, setShouldLoad] = useState<boolean>(false);
 
   function onToggle(id: string) {
     setSelectedIds((prev) =>
       prev.includes(id) ? prev.filter((x) => x != id) : [...prev, id],
     );
   }
+
+  useEffect(() => {
+    async function load() {
+      if (!shouldLoad) {
+        return;
+      }
+      const response = await fetch("/cookbook_recipes.xml");
+      const text = await response.text();
+      setText(text);
+    }
+    load();
+  }, [shouldLoad]);
 
   const recipes: RecipeSummary[] = [
     { id: "1", name: "first" },
@@ -27,6 +41,10 @@ function App() {
           onToggle={onToggle}
         ></RecipeRow>
       ))}
+      <button disabled={shouldLoad} onClick={() => setShouldLoad(true)}>
+        Load recipes
+      </button>
+      <p>{text}</p>
     </div>
   );
 }
